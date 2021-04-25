7968a02d519fe01305cc70f155ac4165fa658bde
morrischen1 <76228520+morrischen1@users.noreply.github.com>
2021-04-25 13:47:54 +0200

    Update timer.py

--- timer.py
+++ timer.py
@@ -27,86 +27,86 @@ s_add = M5Btn(text='+', x=197, y=10, w=50, h=50, bg_c=0xFFFFFF, text_c=0x000000,
 m_red = M5Btn(text='-', x=136, y=119, w=50, h=50, bg_c=0xFFFFFF, text_c=0x000000, font=FONT_MONT_14, parent=None)
 s_red = M5Btn(text='-', x=213, y=119, w=50, h=50, bg_c=0xFFFFFF, text_c=0x000000, font=FONT_MONT_14, parent=None)
 start = M5Btn(text='start', x=130, y=186, w=70, h=30, bg_c=0xFFFFFF, text_c=0x000000, font=FONT_MONT_14, parent=None)
-touch_button0 = M5Btn(text='Button', x=60, y=40, w=200, h=200, bg_c=0xFFFFFF, text_c=0x000000, font=FONT_MONT_14, parent=None)
+touch_button0 = M5Btn(text='Button', x=60, y=40, w=200, h=200, bg_c=0xFFFFFF, text_c=0x000000, font=FONT_MONT_14, parent=None) #dismiss button
 
 from numbers import Number #import library for numbers
  
 def h_add_pressed(): #function for increasing value of hours
   global count, seconds, hours, minutes, vibration_count #import all variable into function
-  if hours >= 99: #if adding on more than 99 hrs then set it to 0 hours instead of 100hrs
+  if hours >= 99: #if adding on more than 99 hrs then set it to 0 hours instead of 100 hrs
     hours = 0
-  else: #else increase by 1 as normal
+  else: #else increase hours by 1 as normal
     hours = (hours if isinstance(hours, Number) else 0) + 1
   pass
-h_add.pressed(h_add_pressed) #when the increase hours button in pressed
+h_add.pressed(h_add_pressed) #determine when increase hrs button is pressed
 
-def m_add_pressed():
+def m_add_pressed(): #function for increasing value of minutes
   global count, seconds, hours, minutes, vibration_count
-  if minutes >= 59:
+  if minutes >= 59: #if adding on more than 59 min then set it to 0 min instead of 60 min
     minutes = 0
-  else:
+  else: #else increase minutes by 1 as normal
     minutes = (minutes if isinstance(minutes, Number) else 0) + 1
   pass
-m_add.pressed(m_add_pressed)
+m_add.pressed(m_add_pressed) #determine when increase min button is pressed
 
-def s_add_pressed():
+def s_add_pressed(): #function for increasing value of seconds
   global count, seconds, hours, minutes, vibration_count
-  if seconds >= 59:
+  if seconds >= 59: #if adding on more than 59 sec then set it to 0 sec instead of 60 sec
     seconds = 0
-  else:
+  else: #else increase seconds by 1 as normal
     seconds = (seconds if isinstance(seconds, Number) else 0) + 1
   pass
-s_add.pressed(s_add_pressed)
+s_add.pressed(s_add_pressed) #determine when increase sec button is pressed
 
-def h_reduce_pressed():
+def h_reduce_pressed(): #function for decreasing value of hours
   global count, seconds, hours, minutes, vibration_count
-  if hours <= 0:
+  if hours <= 0: #if reducing more than 0 hours then set it to 99 hours instead of -1 hours
     hours = 99
-  else:
+  else: #else decrease hours by 1 as normal
     hours = (hours if isinstance(hours, Number) else 0) + -1
   pass
-h_reduce.pressed(h_reduce_pressed)
+h_reduce.pressed(h_reduce_pressed) #determine when decrease hrs button is pressed
 
-def m_red_pressed():
+def m_red_pressed(): #function for decreasing value of minutes
   global count, seconds, hours, minutes, vibration_count
-  if minutes <= 0:
+  if minutes <= 0: #if reducing more than 0 minutes then set it to 59 minutes instead of -1 minutes
     minutes = 59
-  else:
+  else: #else decrease minutes by 1 as normal
     minutes = (minutes if isinstance(minutes, Number) else 0) + -1
   pass
-m_red.pressed(m_red_pressed)
+m_red.pressed(m_red_pressed) #determine when decrease min button is pressed
 
-def s_red_pressed():
+def s_red_pressed(): #function for decreasing value of seconds
   global count, seconds, hours, minutes, vibration_count
-  if seconds <= 0:
+  if seconds <= 0: #if reducing more than 0 seconds then set it to 59 seconds instead of -1 seconds
     seconds = 59
-  else:
+  else: #else decrease seconds by 1 as normal
     seconds = (seconds if isinstance(seconds, Number) else 0) + -1
   pass
-s_red.pressed(s_red_pressed)
+s_red.pressed(s_red_pressed) #determine when decrease sec button is pressed
 
-def start_pressed():
+def start_pressed(): #function for start button
   global count, seconds, hours, minutes, vibration_count
-  count = (count if isinstance(count, Number) else 0) + 1
+  count = (count if isinstance(count, Number) else 0) + 1 #increase variable count when start buttons is pressed
   pass
-start.pressed(start_pressed)
+start.pressed(start_pressed) #determine when start button is pressed
 
-def touch_button0_pressed():
+def touch_button0_pressed(): #function for dismissing the vibration alarm
   global count, seconds, hours, minutes, vibration_count
-  touch_button0.set_hidden(True)
-  vibration_count = (vibration_count if isinstance(vibration_count, Number) else 0) + 1
+  touch_button0.set_hidden(True) #hide the button
+  vibration_count = (vibration_count if isinstance(vibration_count, Number) else 0) + 1 #add 1 to the vibration_count is pressed to turn off the vibration caused when timer runs out 
   power.setVibrationEnable(False)
   pass
-touch_button0.pressed(touch_button0_pressed)
+touch_button0.pressed(touch_button0_pressed) #determine when dismiss button is pressed
 
-
-seconds = 0
-minutes = 0
-hours = 0
-count = 0
-vibration_count = 1
-touch_button0.set_hidden(True)
-while True:
+#set variables to 0
+seconds = 0 #will be used to display the second figure
+minutes = 0 #will be used to display the minute figure
+hours = 0 #will be used to display the hour figure
+count = 0 #will count the pressing of start button
+vibration_count = 1 #set vibration_count to 1, ready to vibrate when timer is activated and runs out, dismiss buttons will disable the vibrator
+touch_button0.set_hidden(True)  #hide dismiss button
+while True: #while loop
   if count % 2 == 1:
     if hours >= 1 and minutes == 0 and seconds == 0:
       minutes = 59
